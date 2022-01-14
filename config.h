fec19edc482d37007e561d50ef21bf05aafa82fc
Syuq <tatd.wine@gmail.com>
2022-01-14 13:07:12 +0700

    modified config.h

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ static const Block blocks[] = {
 	{"",	"sb-battery",	5,	3},
 	/* {"",	"sb-clock",	60,	1}, */
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	/* {"",	"sb-help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
