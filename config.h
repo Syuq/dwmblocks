8a6fcd02a635f3db0347e55fe8672ce4e57ebd33
Syuq <tatd.wine@gmail.com>
2023-06-01 17:05:43 +0700

    🎨 style(config.h): update cryptocurrency displayed in status bar The displayed cryptocurrency in the status bar has been updated from Dogecoin to ShibaInu. This change is purely cosmetic and does not affect the functionality of the program.

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const Block blocks[] = {
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
-	{"",	"sb-price doge Dogecoin 🐕",			9000,	22},
+	{"",	"sb-price shib ShibaInu 🐕",			9000,	22},
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
 	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
 	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
